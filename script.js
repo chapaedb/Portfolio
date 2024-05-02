03c83c1d15c787023e0a6a59c3fc3691875d790f
chapaedb <chapaeresso1@gmail.com>
2024-05-02 23:08:23 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var swiper = new Swiper('.review-slider', {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
